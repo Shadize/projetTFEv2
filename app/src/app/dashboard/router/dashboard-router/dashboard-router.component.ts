853ac8251278447f5fb4e50ded3f678113b6552b
studerd <lledent.n@gmail.com>
2024-06-10 20:15:36 +0200

    directive confirmation

--- app/src/app/dashboard/router/dashboard-router/dashboard-router.component.ts
+++ app/src/app/dashboard/router/dashboard-router/dashboard-router.component.ts
@@ -6,6 +6,11 @@ import {ApiService} from '@api';
 import {SecurityService} from '@security';
 import {TranslateModule} from '@ngx-translate/core';
 
+interface DashboardMenuItem{
+  link:string;
+  icon:string;
+  label:string;
+}
 @Component({
   selector: 'app-dashboard-router',
   standalone: true,
@@ -16,4 +21,11 @@ import {TranslateModule} from '@ngx-translate/core';
 export class DashboardRouterComponent {
   protected securityService:SecurityService = inject(SecurityService);
   routes= AppRoutes;
+  showMobileMenu:boolean =true;
+  menu:DashboardMenuItem[] = [
+    {link:this.routes.SHELVE_LIST, icon:'fa-shelves', label:'header.title.shelve' },
+    {link:this.routes.PRODUCT_LIST, icon:'fa-grid-horizontal', label:'header.title.product' },
+    {link:this.routes.CONSUMPTION_LIST, icon:'fa-person-carry-box', label:'header.title.consumption' },
+    {link:this.routes.MEMBER_LIST, icon:'fa-users-gear', label:'header.title.member' }
+  ]
 }
